4e870f13262a2ca363bdd43ca1dcf4b3f24bb43b
Jason Tang <jtang073@ucr.edu>
2021-08-10 13:56:05 -0700

    Finished cart management project

---
+++ Phase2/CartManagement/README.md
@@ -0,0 +1,11 @@
+# Cart Management Format
+Main Page:
+![Home](/Phase2/CartManagement/images/cmHome.png)
+
+
+Adding Items: (Adding Items will increase the cart size and will be stored in sessionStorage)
+![Add](/Phase2/CartManagement/images/cmAdd.png)
+
+
+Shopping Cart Page: (Also shows total price for entire cart)
+![Cart](/Phase2/CartManagement/images/cmCart.png)
---
+++ Phase2/CartManagement/cartManagement.ts
@@ -0,0 +1,42 @@
+var cartValuea = 15;
+var itemJsona = [];
+
+function addData(item:string, price:number) {
+    itemJsona.push(item);
+    let quantity = 0;
+    if (sessionStorage.length > 0) {
+        for (let i = 0; i < sessionStorage.length; ++i) {
+            let tempJson = JSON.parse(sessionStorage.getItem(sessionStorage.key(i)));
+            if (tempJson.item == String(item)) {
+                quantity = tempJson.quantity;
+            }
+        }
+    }
+    ++quantity;
+    let tempString = "{\"item\": \"" + item.toString() + "\", \"price\": \"" + price.toString() + "\", \"quantity\": \"" + quantity.toString() + "\"}";
+    sessionStorage.setItem(String(item), tempString);
+    updateCart();
+}
+
+function updateCart() {
+    let cartTotal = 0;
+    for (let i = 0; i < sessionStorage.length; ++i) {
+		let tempJson = JSON.parse(sessionStorage.getItem(sessionStorage.key(i)));
+		cartTotal += parseInt(tempJson.quantity);
+	}
+    document.getElementById("cartValue").innerHTML = String(cartTotal);
+}
+
+function displayData() {
+	document.write("<div style=\"margin-top: 150px; text-align: center\"><div><b style=\"font-size:48px;\">Checkout</b><br/><br/><br/><p style=\"color: blue; font-size: 28px;\">Cart</p><center><table border=2px style=\"text-align: center; font-size: 28px;\"><tr><td>Item</td><td>Price</td><td>Quantity</td></tr>")
+	let totalCost = 0;
+	for (let i = 0; i < sessionStorage.length; ++i) {
+		let tempJson = JSON.parse(sessionStorage.getItem(sessionStorage.key(i)));
+		document.write("<tr><td>" + tempJson.item);
+		document.write("<td>$" + tempJson.price);
+		document.write("<td>" + tempJson.quantity + "<tr/>");
+		totalCost += Number(tempJson.price * tempJson.quantity);
+	}
+	document.write("</table><br/><p style=\"font-size: 28px\"> Total Cost: $" + totalCost + "</p><input type=\"button\" value=\"Back\" style=\"margin-right: 345px; font-size: 18px;\" onclick=\"window.location='cmIndex.html';\"/></center></div></div>");
+	return true;
+}
